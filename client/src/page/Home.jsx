9dc44141f9a84a95089adeeadd8a081b84c61054
tsukipond8531 <reader2027@gmail.com>
2023-05-25 12:00:00 +0700

    feat: update home

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
+      <h1 className="text-5xl p-3">AvaxGods</h1>
       <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+      <p className="text-xl p-3">Progjar Ganas</p>
     </div>
   )
 };
