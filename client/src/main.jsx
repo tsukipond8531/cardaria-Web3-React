fa8f3e3b99547b56e2cb0bf153d1d090b477d04d
tsukipond8531 <reader2027@gmail.com>
2023-06-01 12:00:00 +0700

    feat: modular route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './page/Home';
+import { Home, CreateBattle } from './page';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/create-battle" element={<CreateBattle />} />
     </Routes>
   </BrowserRouter>,
 );
---
+++ client/src/page/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import CreateBattle from "./CreateBattle";
+
+export {
+    Home,
+    CreateBattle
+}
