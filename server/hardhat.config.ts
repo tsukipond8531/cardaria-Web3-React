53a7d7dcc59f57ae4b6b74ebbfa54bcc53c7c5d4
tsukipond8531 <reader2027@gmail.com>
2023-06-11 00:00:00 +0700

    ci: hardhat config

--- server/hardhat.config.ts
+++ server/hardhat.config.ts
@@ -1,8 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+import '@nomiclabs/hardhat-ethers';
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-};
+dotenv.config();
 
-export default config;
+//* Notes for deploying the smart contract on your own subnet
+//* More info on subnets: https://docs.avax.network/subnets
+//* Why deploy on a subnet: https://docs.avax.network/subnets/when-to-use-subnet-vs-c-chain
+//* How to deploy on a subnet: https://docs.avax.network/subnets/create-a-local-subnet
+//* Transactions on the C-Chain might take 2-10 seconds -> the ones on the subnet will be much faster
+//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the subnet we can create our own token
+//* You are in complete control over the network and it's inner workings
+
+export default {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    // subnet: {
+    //   url: process.env.NODE_URL,
+    //   chainId: Number(process.env.CHAIN_ID),
+    //   gasPrice: 'auto',
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
+}
