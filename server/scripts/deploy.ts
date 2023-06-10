d966d034b8e38d75279d21fa202950c73f810751
tsukipond8531 <reader2027@gmail.com>
2023-06-11 00:00:00 +0700

    feat: update deploy script

--- server/scripts/deploy.ts
+++ server/scripts/deploy.ts
@@ -1,27 +1,27 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+import console from 'console';
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const _metadataUri = 'https://gateway.pinata.cloud/ipfs/https://gateway.pinata.cloud/ipfs/QmX2ubhtBPtYw75Wrpv6HLb1fhbJqxrnbhDo1RViW3oVoi';
 
-  const lockedAmount = ethers.parseEther("0.001");
+async function deploy(name: string, ...params: [string]) {
+  const contractFactory = await ethers.getContractFactory(name);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  return await contractFactory.deploy(...params).then((f) => f.deployed());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function main() {
+  const [admin] = await ethers.getSigners();
+  
+  console.log(`Deploying a smart contract...`);
+
+  const AVAXGods = (await deploy('AVAXGods', _metadataUri)).connect(admin);
+
+  console.log({ AVAXGods: AVAXGods.address });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  });
