e6dc7164c69e1bf4df2be642256397cefe553a55
tsukipond8531 <reader2027@gmail.com>
2023-06-12 12:22:25 +0700

    feat: create game load component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,12 @@ import PageHOC from "./PageHOC";
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
 import Alert from "./Alert";
+import GameLoad from "./GameLoad";
 
 export {
     PageHOC,
     CustomInput,
     CustomButton,
-    Alert
+    Alert,
+    GameLoad
 }
