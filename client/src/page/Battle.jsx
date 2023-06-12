5f903cd4fbd4d20b9b80364196ded7c394fac481
tsukipond8531 <reader2027@gmail.com>
2023-06-12 19:21:50 +0700

    feat: get player info

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -13,9 +13,51 @@ const Battle = () => {
     const { battleName } = useParams();
 
     const navigate = useNavigate();
+    console.log('p1',player1)
+    console.log('p1',player2)
+
+    useEffect(() => {
+        const getPlayerInfo = async () => {
+          try {
+            let player01Address = null;
+            let player02Address = null;
+    
+            if (gameData.activeBattle.players[0].toLowerCase() === walletAddress.toLowerCase()) {
+              player01Address = gameData.activeBattle.players[0];
+              player02Address = gameData.activeBattle.players[1];
+            } else {
+              player01Address = gameData.activeBattle.players[1];
+              player02Address = gameData.activeBattle.players[0];
+            }
+    
+            const p1TokenData = await contract.getPlayerToken(player01Address);
+            const player01 = await contract.getPlayer(player01Address);
+            const player02 = await contract.getPlayer(player02Address);
+    
+            const p1Att = p1TokenData.attackStrength.toNumber();
+            const p1Def = p1TokenData.defenseStrength.toNumber();
+            const p1H = player01.playerHealth.toNumber();
+            const p1M = player01.playerMana.toNumber();
+            const p2H = player02.playerHealth.toNumber();
+            const p2M = player02.playerMana.toNumber();
+    
+            setPlayer1({ ...player01, att: p1Att, def: p1Def, health: p1H, mana: p1M });
+            setPlayer2({ ...player02, att: 'X', def: 'X', health: p2H, mana: p2M });
+          } catch (error) {
+            console.log(error);
+            // setErrorMessage(error.message);
+          }
+        };
+    
+        if (contract && gameData.activeBattle) getPlayerInfo();
+      }, [contract, gameData, battleName]);
+
+      console.log({contract, gameData, battleName})
 
   return (
     <div className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}>
+        {showAlert?.status && <Alert type={showAlert.type} message={showAlert.message} />}
+
         <h1 className='text-xl text-black'>{battleName}</h1>
     </div>
   )
