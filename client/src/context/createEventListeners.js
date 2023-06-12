fdd57e1e53bb63ffb5e2174690ad7aa6f302b340
tsukipond8531 <reader2027@gmail.com>
2023-06-12 12:55:07 +0700

    feat: new battle event handler

--- client/src/context/createEventListeners.js
+++ client/src/context/createEventListeners.js
@@ -18,6 +18,7 @@ const AddNewEvent = (eventFilter, provider, cb) => {
     provider, 
     walletAddress, 
     setShowAlert, 
+    setUpdateGameData
     // player1Ref, player2Ref, setUpdateGameData 
     }) => {
     const NewPlayerEventFilter = contract.filters.NewPlayer();
@@ -32,4 +33,17 @@ const AddNewEvent = (eventFilter, provider, cb) => {
         });
       }
     })
+
+    const NewBattleEventFilter = contract.filters.NewBattle();
+    AddNewEvent(NewBattleEventFilter, provider, ({ args }) => {
+      console.log('New battle started!', args, walletAddress);
+  
+      if (walletAddress.toLowerCase() === args.player1.toLowerCase() || walletAddress.toLowerCase() === args.player2.toLowerCase()) {
+        navigate(`/battle/${args.battleName}`);
+      }
+  
+      setUpdateGameData((prevUpdateGameData) => prevUpdateGameData + 1);
+    });
+
+
   };
