de0c169beddcbd271b9c595c8cc78852e5b86449
tsukipond8531 <reader2027@gmail.com>
2023-06-13 13:40:42 +0700

    feat: custom error handling

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -7,7 +7,7 @@ import { attack, attackSound, defense, defenseSound, player01 as player01Icon, p
 import { playAudio } from '../utils/animation.js';
 
 const Battle = () => {
-    const { contract, gameData, walletAddress, showAlert, setShowAlert, battleGround } = useGlobalContext();
+    const { contract, gameData, walletAddress, showAlert, setShowAlert, battleGround, setErrorMessage } = useGlobalContext();
     const [player2, setPlayer2] = useState({});
     const [player1, setPlayer1] = useState({});
     const { battleName } = useParams();
@@ -42,8 +42,7 @@ const Battle = () => {
             setPlayer1({ ...player01, att: p1Att, def: p1Def, health: p1H, mana: p1M });
             setPlayer2({ ...player02, att: 'X', def: 'X', health: p2H, mana: p2M });
           } catch (error) {
-            console.log("error", error);
-            // setErrorMessage(error.message);
+            setErrorMessage(error.message);
           }
         };
     
@@ -62,12 +61,8 @@ const Battle = () => {
             message: `Initiating ${choice === 1 ? 'attack' : 'defense'}`,
           });
         } catch (error) {
-          setShowAlert({
-            status: true,
-            type: 'failure',
-            message: `Error`,
-          });
           console.log(error)
+          setErrorMessage(error.message);
         }
       };
 
